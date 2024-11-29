58255f4ebce1334a97943917ad1a7f78927a0cbb
James1Law <james.law@90poe.io>
2024-11-29 15:01:37 +0000

    font and more snow

--- src/components/Snow.jsx
+++ src/components/Snow.jsx
@@ -4,7 +4,7 @@ import '../styles/Snow.css';
 const Snow = () => {
   return (
     <div className="snow-container">
-      {[...Array(50)].map((_, index) => (
+      {[...Array(180)].map((_, index) => (
         <div key={index} className="snow" />
       ))}
     </div>
