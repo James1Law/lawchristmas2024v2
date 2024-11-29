b1dc73d12dd43a1e70a6f50009e08566f68fa08f
James1Law <james.law@90poe.io>
2024-11-29 15:23:11 +0000

    more snow

--- src/components/Snow.jsx
+++ src/components/Snow.jsx
@@ -2,10 +2,22 @@ import React from 'react';
 import '../styles/Snow.css';
 
 const Snow = () => {
+  const getRandomPosition = () => {
+    return {
+      left: `${Math.random() * 100}%`,
+      animationDuration: `${Math.random() * 10 + 5}s`,
+      animationDelay: `-${Math.random() * 5}s`
+    };
+  };
+
   return (
     <div className="snow-container">
-      {[...Array(180)].map((_, index) => (
-        <div key={index} className="snow" />
+      {[...Array(250)].map((_, index) => (
+        <div 
+          key={index} 
+          className="snow" 
+          style={getRandomPosition()}
+        />
       ))}
     </div>
   );
